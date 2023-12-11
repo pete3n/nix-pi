272fff3b98b2c80ad40af4adb1d9b49d0f69e229
Pete <pete3n@protonmail.com>
2023-12-11 18:01:22 -0500

    Wireless environment build script

--- build-env.sh
+++ build-env.sh
@@ -1,10 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-read -p "Enter SSID for the configuration network: " ssid
-read -s -p "Enter password for the configuration network: " password
+IMAGE_NAME="${1:-zero2w-nixos-k6.6.5.img}"
+IMAGE_PATH="./result/sd-image/$IMAGE_NAME"
+WIRELESS_ENV_PATH="wireless.env"
+MOUNT_DIR="/tmp/sd-image"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run with sudo privileges."
+    exit 1
+fi
+
+read -p "Enter SSID for the provisioning network: " ssid
+read -s -p "Enter password for the provisioning network: " password
 echo 
 
-echo "CONFIG_NET_SSID=\"$ssid\"" > .env
-echo "CONFIG_NET_PASSWORD=\"$password\"" >> .env
+echo "PROVISION_SSID=\"$ssid\"" > ${WIRELESS_ENV_PATH}
+echo "PROVISION_PASS=\"$password\"" >> ${WIRELESS_ENV_PATH}
 
-echo "SSID and password have been saved to .env."
+echo "SSID and password have been saved to ${WIRELESS_ENV_PATH}"
+
+LOOP_DEVICE=$(losetup -Pf --show "$IMAGE_PATH")
+if [ -z "$LOOP_DEVICE" ]; then
+    echo "Failed to create loop device."
+    exit 1
+fi
+
+mkdir -p "$MOUNT_DIR"
+
+if ! mount "${LOOP_DEVICE}p1" "$MOUNT_DIR"; then
+    echo "Failed to mount the first partition of $LOOP_DEVICE"
+    losetup -d "$LOOP_DEVICE"
+    exit 1
+fi
+
+if ! cp "$WIRELESS_ENV_PATH" "$MOUNT_DIR/"; then
+    echo "Failed to copy $WIRELESS_ENV_PATH to $MOUNT_DIR"
+    umount "$MOUNT_DIR"
+    losetup -d "$LOOP_DEVICE"
+    exit 1
+fi
+
+umount "$MOUNT_DIR"
+rmdir "$MOUNT_DIR"
+losetup -d "$LOOP_DEVICE"
+
+echo "wireless.env copied successfully to $IMAGE_PATH"
