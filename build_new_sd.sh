88c661db3468d0b99f0e728ed3b334314133d970
Pete <pete3n@protonmail.com>
2023-12-15 16:39:02 -0500

    ro image workaround

--- build_new_sd.sh
+++ build_new_sd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IMAGE_NAME="${1:-zero2w-nixos-k6.6.5.img}"
 IMAGE_PATH="./result/sd-image/$IMAGE_NAME"
-MOUNT_DIR="/tmp/sd-image"
+MOUNT_DIR="./image"
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run with sudo privileges."
@@ -75,16 +75,20 @@ touch /run/secrets/provision_net_pass.age
 echo
 echo "Adding admin public key to authorized keys in config..."
 admin_pub_key=$(cat ./private/admin.pub)
-
+echo
 replacement_ssh="users.users.admin.openssh.authorizedKeys.keys = [ \"$admin_pub_key\" ];"
 sed -i "s|users.users.admin.openssh.authorizedKeys.keys = \[.*\];|$replacement_ssh|" ./zero2w.nix
-
+echo
 echo "Building SD card image..."
 nix build .#images.zero2w
 echo
+echo "Copying result image..."
+cp $IMAGE_PATH ./
+echo
+chmod +w $IMAGE_NAME
+echo
 echo "Mounting image..."
-
-LOOP_DEVICE=$(losetup -Pf --show "$IMAGE_PATH")
+LOOP_DEVICE=$(losetup -Pf --show "$IMAGE_NAME")
 if [ -z "$LOOP_DEVICE" ]; then
     echo "Failed to create loop device."
     exit 1
@@ -93,25 +97,27 @@ fi
 mkdir -p "$MOUNT_DIR"
 
 if ! mount "${LOOP_DEVICE}p2" "$MOUNT_DIR"; then
-    echo "Failed to mount the first partition of $LOOP_DEVICE"
+    echo "Failed to mount the second partition of $LOOP_DEVICE"
     losetup -d "$LOOP_DEVICE"
     exit 1
 fi
 
 echo
 echo "Copying ssh keys..."
-if ! cp "./private/zero2w" "$MOUNT_DIR/root/.ssh/"; then
-    echo "Failed to copy private system SSH key to $MOUNT_DIR/root/.ssh"
-    ehco "Umounting image..." 
+if ! cp "./private/zero2w" "$MOUNT_DIR/boot/"; then
+    echo "Failed to copy private system SSH key to $MOUNT_DIR/boot/"
+    echo "Umounting image..." 
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
+    rmdir "$MOUNT_DIR"
     exit 1
 fi
-if ! cp "./private/admin" "$MOUNT_DIR/home/admin/.ssh/"; then
-    echo "Failed to copy private admin SSH key to $MOUNT_DIR/home/admin/.ssh"
+if ! cp "./private/admin" "$MOUNT_DIR/boot/"; then
+    echo "Failed to copy private admin SSH key to $MOUNT_DIR/boot/"
     echo "Unmounting image..."
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
+    rmdir "$MOUNT_DIR"
     exit 1
 fi
 
@@ -120,7 +126,10 @@ echo "Unmounting image..."
 umount "$MOUNT_DIR"
 rmdir "$MOUNT_DIR"
 losetup -d "$LOOP_DEVICE"
+
 echo
-echo "Image creation complete. You can copy your image with a command similar to:"
-echo "sudo dd of=/dev/mmcblk0 if=./result/sd-card/zero2w-nixos-k6.6.5.img bs=1M status=progress"
+echo "Image creation complete"
+echo 
+ehco "You can copy your image with a command similar to:"
+echo "sudo dd of=/dev/mmcblk0 if=./zero2w-nixos-k6.6.5.img bs=1M status=progress"
 echo "CAUTION: Confirm your SD card device is /dev/mmcblk0 before using this command"
