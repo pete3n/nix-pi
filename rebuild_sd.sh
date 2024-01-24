00e8a643c9949d89a2f6db6da2636ade8c0d46a5
Pete <pete3n@protonmail.com>
2024-01-24 13:39:09 -0500

    Fixed permissions issues, added error handler

--- rebuild_sd.sh
+++ rebuild_sd.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#!/bin/bash 
 #
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -24,48 +24,60 @@ case $exit_status in
         ;;
     *)
         echo "Error: Invalid environment or check_env.sh script not found."
+        ./restore_env.sh
         exit 1
         ;;
 esac
 
-IMAGE_NAME="${1:-zero2w-nixos-k6.6.5.img}"
-IMAGE_PATH="./result/sd-image/$IMAGE_NAME"
+error_handler() {
+    exit_status=$?
+    if [[ $exit_status -ne 0 ]]; then
+        echo "Script halted due to error"
+        echo
+        echo "Reverting git..."
+        git reset --hard
+        ./restore_env.sh
+        exit 1
+    fi
+}
+trap 'error_handler $LINENO' ERR
+
+IMAGE_NAME="zero2w-nixos-k6.6.5"
+IMAGE_EXT=".img"
+IMAGE_PATH="./result/sd-image/$IMAGE_NAME$IMAGE_EXT"
 MOUNT_DIR="./image"
 OUTPUT_DIR="./output"
+BUILD_TIMESTAMP=$(date +"%d-%m-%y-%H-%M-%S")
 
-# Copy age secrets files to working directory so nix can build them
-echo "Creating /run/zero2w-build-secrets build placeholders..."
-mkdir -p /run/zero2w-build-secrets
-cp ./secrets/*.age /run/zero2w-build-secrets/
+# Track encrypted secrets with Git so they can be added to the store
+echo
+echo "Adding encrypted secrets to git..."
+sudo -u $SUDO_USER  git add ./secrets/*.age
 echo
 
 echo "Building SD card image..."
-$NIX --extra-experimental-features nix-command --extra-experimental-features flakes build .#images.zero2w
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-    echo "Error detected in build process, exiting..."
-    exit $exit_status
-fi
+sudo -u $SUDO_USER $NIX --extra-experimental-features "nix-command flakes" build -Lv .#images.zero2w
 echo
-
 echo "Copying result image..."
-mkdir -p $OUTPUT_DIR
-cp $IMAGE_PATH ${OUTPUT_DIR}/$IMAGE_NAME
+sudo -u $SUDO_USER mkdir -p $OUTPUT_DIR
+sudo -u $SUDO_USER cp $IMAGE_PATH ${OUTPUT_DIR}/"$IMAGE_NAME"-"$BUILD_TIMESTAMP""$IMAGE_EXT"
 echo
-chmod +w ${OUTPUT_DIR}/$IMAGE_NAME
+chmod +w ${OUTPUT_DIR}/"$IMAGE_NAME"-"$BUILD_TIMESTAMP""$IMAGE_EXT"
 echo
 
 echo "Mounting image..."
-LOOP_DEVICE=$(losetup -Pf --show "${OUTPUT_DIR}/$IMAGE_NAME")
+LOOP_DEVICE=$(losetup -Pf --show "${OUTPUT_DIR}/$IMAGE_NAME-$BUILD_TIMESTAMP$IMAGE_EXT")
 if [ -z "$LOOP_DEVICE" ]; then
     echo "Failed to create loop device."
+    ./restore_env.sh
     exit 1
 fi
 
-mkdir -p $MOUNT_DIR
+sudo -u $SUDO_USER mkdir -p $MOUNT_DIR
 if ! mount "${LOOP_DEVICE}p2" "$MOUNT_DIR"; then
     echo "Failed to mount the second partition of $LOOP_DEVICE"
     losetup -d "$LOOP_DEVICE"
+    ./restore_env.sh
     exit 1
 fi
 echo
@@ -77,6 +89,7 @@ if ! cp "./private/zero2w" "$MOUNT_DIR/boot/"; then
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
     rmdir "$MOUNT_DIR"
+    ./restore_env.sh
     exit 1
 fi
 if ! cp "./private/admin" "$MOUNT_DIR/boot/"; then
@@ -85,6 +98,7 @@ if ! cp "./private/admin" "$MOUNT_DIR/boot/"; then
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
     rmdir "$MOUNT_DIR"
+    ./restore_env.sh
     exit 1
 fi
 
@@ -95,12 +109,10 @@ rmdir "$MOUNT_DIR"
 losetup -d "$LOOP_DEVICE"
 echo
 
-echo "Cleaning up temporary build secrets..."
-rm /run/zero2w-build-secrets -rf
-
+./restore_env.sh
 echo
 echo "Image creation complete"
 echo 
 echo "You can copy your image with a command similar to:"
-echo "sudo dd of=/dev/mmcblk0 if=./output/zero2w-nixos-k6.6.5.img bs=1M status=progress"
+echo "sudo dd of=/dev/mmcblk0 if=./output/$IMAGE_NAME-$BUILD_TIMESTAMP$IMAGE_EXT bs=1M status=progress"
 echo "CAUTION: Confirm your SD card device is /dev/mmcblk0 before using this command"
