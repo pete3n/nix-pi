de3c3171b02beca40780a86e80baff3cef1bff70
Pete <pete3n@protonmail.com>
2024-01-24 13:30:53 -0500

    Fixed permissions issues and error handling

--- build_new_sd.sh
+++ build_new_sd.sh
@@ -24,14 +24,30 @@ case $exit_status in
         ;;
     *)
         echo "Error: Invalid environment or check_env.sh script not found."
+        ./restore_env.sh
         exit 1
         ;;
 esac
 
-IMAGE_NAME="${1:-zero2w-nixos-k6.6.5.img}"
-IMAGE_PATH="./result/sd-image/$IMAGE_NAME"
+error_handler() {
+    exit_status=$?
+    if [[ $exit_status -ne 0 ]]; then
+        echo "Script halted due to error"
+        echo
+        echo "Reverting git..."
+        git reset --hard
+        ./restore_env.sh
+        exit 1
+    fi
+}
+trap 'error_handler $LINENO' ERR
+
+IMAGE_NAME="zero2w-nixos-k6.6.5"
+IMAGE_EXT=".img"
+IMAGE_PATH="./result/sd-image/$IMAGE_NAME$IMAGE_EXT"
 MOUNT_DIR="./image"
 OUTPUT_DIR="./output"
+BUILD_TIMESTAMP=$(date +"%d-%m-%y-%H-%M-%S")
 
 read -p "Enter SSID for the provisioning network: " ssid
 echo
@@ -60,8 +76,8 @@ while true; do
 done
 echo
 echo "Making private and secret directories..."
-mkdir -p ./private
-mkdir -p ./secrets
+sudo -u $SUDO_USER mkdir -p ./private
+sudo -u $SUDO_USER mkdir -p ./secrets
 
 ssh-keygen -t ed25519 -C "zero2w_system_key" -f ./private/zero2w
 ssh-keygen -t ed25519 -C "admin_user_key" -f ./private/admin
@@ -84,12 +100,14 @@ echo "$admin_password" | mkpasswd -m sha-512 -s > ./private/admin_hash
 age -R ./private/zero2w.pub -R ./private/admin.pub -e -i ./private/zero2w -i ./private/admin -o ./secrets/admin_pass.age ./private/admin_hash
 echo "$ssid" | age -R ./private/zero2w.pub -R ./private/admin.pub -e -o ./secrets/provision_net_ssid.age
 echo "$password" | age -R ./private/zero2w.pub -R ./private/admin.pub -e -o ./secrets/provision_net_pass.age
+sudo chown -R $SUDO_USER:$SUDO_GROUP ./secrets
+sudo chown -R $SUDO_USER:$SUDO_GROUP ./private
+chmod 600 ./private/*
 
-# Copy age secrets files to working directory so nix can build them
+# Track encrypted secrets with Git so they can be added to the store
 echo
-echo "Creating /run/zero2w-build-secrets build placeholders..."
-mkdir -p /run/zero2w-build-secrets
-cp ./secrets/*.age /run/zero2w-build-secrets/
+echo "Adding encrypted secrets to git..."
+sudo -u $SUDO_USER git add ./secrets/*
 echo
 
 echo "Adding admin public key to authorized keys in config..."
@@ -97,10 +115,11 @@ admin_pub_key=$(cat ./private/admin.pub)
 echo
 replacement_ssh="users.users.admin.openssh.authorizedKeys.keys = [ \"$admin_pub_key\" ];"
 sed -i "s|users.users.admin.openssh.authorizedKeys.keys = \[.*\];|$replacement_ssh|" ./zero2w.nix
+sudo -u $SUDO_USER git add ./robo-zero2w-image.nix
 echo
 
 echo "Building SD card image..."
-$NIX --extra-experimental-features nix-command --extra-experimental-features flakes build .#images.zero2w
+$NIX --extra-experimental-features "nix-command flakes" build -Lv .#images.zero2w
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "Error detected in build process, exiting..."
@@ -108,16 +127,18 @@ if [ $exit_status -ne 0 ]; then
 fi
 echo
 
-mkdir -p $OUTPUT_DIR
+sudo -u $SUDO_USER mkdir -p $OUTPUT_DIR
 echo "Copying result image..."
-cp $IMAGE_PATH ${OUTPUT_DIR}/$IMAGE_NAME
+sudo -u $SUDO_USER cp $IMAGE_PATH ${OUTPUT_DIR}/"$IMAGE_NAME"-"$BUILD_TIMESTAMP""$IMAGE_EXT"
 echo
-chmod +w ${OUTPUT_DIR}/$IMAGE_NAME
+chown $SUDO_USER ${OUTPUT_DIR}/"$IMAGE_NAME"-"$BUILD_TIMESTAMP""$IMAGE_EXT"
+sudo -u $SUDO_USER chmod +w ${OUTPUT_DIR}/"$IMAGE_NAME"-"$BUILD_TIMESTAMP""$IMAGE_EXT"
 echo
 echo "Mounting image..."
-LOOP_DEVICE=$(losetup -Pf --show "${OUTPUT_DIR}/$IMAGE_NAME")
+LOOP_DEVICE=$(losetup -Pf --show "${OUTPUT_DIR}/$IMAGE_NAME-$BUILD_TIMESTAMP$IMAGE_EXT")
 if [ -z "$LOOP_DEVICE" ]; then
     echo "Failed to create loop device."
+    ./restore_env.sh
     exit 1
 fi
 
@@ -125,6 +146,7 @@ mkdir -p "$MOUNT_DIR"
 if ! mount "${LOOP_DEVICE}p2" "$MOUNT_DIR"; then
     echo "Failed to mount the second partition of $LOOP_DEVICE"
     losetup -d "$LOOP_DEVICE"
+    ./restore_env.sh
     exit 1
 fi
 echo
@@ -136,6 +158,7 @@ if ! cp "./private/zero2w" "$MOUNT_DIR/boot/"; then
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
     rmdir "$MOUNT_DIR"
+    ./restore_env.sh
     exit 1
 fi
 if ! cp "./private/admin" "$MOUNT_DIR/boot/"; then
@@ -144,6 +167,7 @@ if ! cp "./private/admin" "$MOUNT_DIR/boot/"; then
     umount "$MOUNT_DIR"
     losetup -d "$LOOP_DEVICE"
     rmdir "$MOUNT_DIR"
+    ./restore_env.sh
     exit 1
 fi
 echo
@@ -157,9 +181,10 @@ echo
 echo "Cleaning up temporary build secrets..."
 rm /run/zero2w-build-secrets -rf
 
+./restore_env.sh
 echo
 echo "Image creation complete"
 echo 
 echo "You can copy your image with a command similar to:"
-echo "sudo dd of=/dev/mmcblk0 if=./output/zero2w-nixos-k6.6.5.img bs=1M status=progress"
+echo "sudo dd of=/dev/mmcblk0 if=./output/$IMAGE_NAME-$BUILD_TIMESTAMP$IMAGE_EXT bs=1M status=progress"
 echo "CAUTION: Confirm your SD card device is /dev/mmcblk0 before using this command"
