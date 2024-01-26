94b0e717feba9072bf9166e82a8c98e3f18bc5ee
Pete <pete3n@protonmail.com>
2024-01-26 13:49:53 -0500

    Added template home-manager config

--- restore_env.sh
+++ restore_env.sh
@@ -8,7 +8,7 @@ ORIGINAL_SHEBANG_LINE2="#"
 for script in "${SCRIPTS[@]}"; do
     if [ -f "$script" ]; then
         # Replace the first two lines with the original shebang lines
-        sed -i "1s|.*|${ORIGINAL_SHEBANG_LINE1}|; 2s|.*|${ORIGINAL_SHEBANG_LINE2}|" "$script"
+        sudo -u $SUDO_USER sed -i "1s|.*|${ORIGINAL_SHEBANG_LINE1}|; 2s|.*|${ORIGINAL_SHEBANG_LINE2}|" "$script"
     fi
 done
 
